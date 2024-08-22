bdafc2e9093a13520ddafd85eb47308107c92be6
Juan Pablo Arr <ju.arriagadar@duocuc.cl>
2024-08-22 09:53:41 -0400

    crear rutas para el navegador-ejemplo:app/home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule/* raiz del sitio */, Routes/* rutas */ } from '@angular/router';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'contacto',
+    component:ContactoComponent
+  },
+  {
+    path:'ayuda',
+    component:AyudaComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
