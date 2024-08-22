495dcb3de18712d74db58a8de4668b50fa8f5cb1
Juan Pablo Arr <ju.arriagadar@duocuc.cl>
2024-08-22 09:45:09 -0400

    contacto/ayuda-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ContactoComponent,
+    AyudaComponent
   ],
   imports: [
     BrowserModule,
